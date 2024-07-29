ec3dd2567808d8101002914e67d5d50282ede678
roovthebee <ravenouszen@gmail.com>
2024-07-29 12:16:11 +0100

    Wall Cling + Changes
    - Added wall cling mechanic
    - Changed running mechanic slightly
    - Added water spout effect to fire hydrant

--- Assets/Assets/Scripts/Environment/FireHydrant.cs
+++ Assets/Assets/Scripts/Environment/FireHydrant.cs
@@ -1,21 +1,43 @@
 
 using UnityEngine;
+using System.Collections;
 using Utility;
 
 namespace Environment {
     public class FireHydrant : MonoBehaviour {
         public float hitboxHeight;
+        public Sprite waterSprite;
+        float lastActive;
+        public GameObject fireHydrantHitbox;
 
         private void Awake() {
-            GameObject fireHydrantHitbox = new GameObject("FireHydrantHitbox");
+            fireHydrantHitbox = new GameObject("FireHydrantHitbox");
             fireHydrantHitbox.transform.position = new Vector3(transform.position.x, transform.position.y + (hitboxHeight / 2), transform.position.z);
-            fireHydrantHitbox.transform.localScale = new Vector3(1, hitboxHeight, 0);
+            fireHydrantHitbox.transform.localScale = new Vector3(1f, hitboxHeight, 0);
             fireHydrantHitbox.transform.parent = transform;
 
             BoxCollider2D boxCollider = fireHydrantHitbox.AddComponent<BoxCollider2D>();
             boxCollider.isTrigger = true;
 
+            SpriteRenderer spriteRenderer = fireHydrantHitbox.AddComponent<SpriteRenderer>();
+            spriteRenderer.color = new Color(0, 1, 1);
+            spriteRenderer.sprite = waterSprite;
+            spriteRenderer.sortingOrder = -5;
+
             fireHydrantHitbox.AddComponent<HydrantCollisionHandler>();
+        }
+
+        private void Update() {
+            if (Time.realtimeSinceStartup - lastActive > 9) {
+                lastActive = Time.realtimeSinceStartup;
+                StartCoroutine(ActivateHydrant());
+            }
+        }
+
+        private IEnumerator ActivateHydrant() {
+            fireHydrantHitbox.SetActive(true);
+            yield return new WaitForSeconds(5);
+            fireHydrantHitbox.SetActive(false);
         }
 
         private Vector2 GetHitboxDimensions() {
